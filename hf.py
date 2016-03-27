73bcc84d99e01d8545987f7b8b6f429ddb09d215
Marcelo M. S. Souza <marcelo.mssouza@gmail.com>
2016-03-27 11:18:19 -0300

    added support for dtw and fastdtw metrics

--- hf.py
+++ hf.py
@@ -4,76 +4,30 @@ import sys
 import oct2py 
 import cPickle
 import numpy as np
-
+import fastdtw
+import dtw
+from functools import partial
 
 def cost(x,y):
- M = x.shape[0]
+ M = len(x)
  c = 0
  for k in np.arange(M):
   c = c + np.abs(x[k] - y[k])/float(min(k+1, M-k+1))
  return c
 
-def dtw(x, y):
-    """
-    Computes Dynamic Time Warping (DTW) of two sequences.
-
-    :param array x: N1*M array
-    :param array y: N2*M array
-    :param func dist: distance used as cost measure
-
-    Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
-    """
-    assert len(x)
-    assert len(y)
-    r, c = len(x), len(y)
-    D0 = np.zeros((r + 1, c + 1))
-    D0[0, 1:] = np.inf
-    D0[1:, 0] = np.inf
-    D1 = D0[1:, 1:] # view
-    for i in range(r):
-        for j in range(c):
-            D1[i, j] = cost(x[i], y[j])
-    C = D1.copy()
-    for i in range(r):
-        for j in range(c):
-            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
-    if len(x)==1:
-        path = np.zeros(len(y)), range(len(y))
-    elif len(y) == 1:
-        path = range(len(x)), np.zeros(len(x))
-    else:
-        path = _traceback(D0)
-    return D1[-1, -1] / sum(D1.shape), C, D1, path
-
-def _traceback(D):
-    i, j = np.array(D.shape) - 2
-    p, q = [i], [j]
-    while ((i > 0) or (j > 0)):
-        tb = np.argmin((D[i, j], D[i, j+1], D[i+1, j]))
-        if (tb == 0):
-            i -= 1
-            j -= 1
-        elif (tb == 1):
-            i -= 1
-        else: # (tb == 2):
-            j -= 1
-        p.insert(0, i)
-        q.insert(0, j)
-    return np.array(p), np.array(q)
-
-def dist(X,Y,beta):
+def dist(X,Y,beta,metric = dtw.dtw):
  CX = np.std(X,axis = 1).mean()
  CY = np.std(Y,axis = 1).mean() 
- return  dtw(X,Y)[0]/(CX + CY + beta)
+ return  metric(X,Y,dist = cost)[0]/(CX + CY + beta)
 
 nc = 100
 k = 4
 beta = 0.0001
 
 db = cPickle.load(open(sys.argv[1]+"classes.txt"))
-idx = np.where(np.array(db.values()) == 1)
-names = db.keys()
-cl = db.values()
+#idx = np.where(np.array(db.values()) == 1)
+names = db.keys()[0:5]
+cl = db.values()[0:5]
 N = len(cl)
 print N
 
@@ -98,21 +52,28 @@ for mt in a:
 
 print "Calculando matriz de distancias"
 md = np.zeros((N,N))
+mf = np.zeros((N,N))
 
+fastdtw = partial(fastdtw.fastdtw,radius = 20)
 for i in np.arange(len(Fl)):
  for j in np.arange(i,len(Fl)):
-  dd = dist(Fl[i],Fl[j],beta)
-  print i,j,dd
+  dd = dist(Fl[i],Fl[j],beta,metric = dtw.dtw)
+  df = dist(Fl[i],Fl[j],beta,metric = fastdtw)
+  print i,j,dd,df
   md[i,j] = dd
+  mf[i,j] = df
 
 md = md + md.T
+mf = mf + mf.T
+print md,mf
+exit(0)
 
 print "Calculando Bulls-eye"
 # Acumulador para contabilizar desempenho do experimento
 tt = 0.
 Nretr = 40
  #print "Calculando bull eye score"
-for i in np.arange(Nobj):
+for i in np.arange(len(md)):
   idx = np.argsort(md[i])
   classe_retrs = (cl[idx])[0:Nretr]
   n = np.nonzero(np.array(classe_retrs) == cl[i])

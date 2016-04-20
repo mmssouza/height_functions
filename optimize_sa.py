corrected similarity calculation to comply with original hf implementation. Added one more parameter in optimization (beta).

optimize_sa.py
@@ -14,7 +14,10 @@ from time import time
 import atexit
 
 def fy(n,nc):
-  oc = oct2py.Oct2Py()
+  try:
+   oc = oct2py.Oct2Py()
+  except:
+   oc = oct2py.Oct2Py()
   oc.addpath("common_HF")
   oc.eval("pkg load statistics;")
   a,b = oc.batch_hf(n,nc)
@@ -26,7 +29,7 @@ if __name__ == '__main__':
  dataset = ""
  fout = ""
  dim = -1
- NS = 2
+ NS = 3
  
  try:                                
   opts,args = getopt.getopt(sys.argv[1:], "o:d:", ["mt=","dim=","output=","dataset="])
@@ -64,8 +67,10 @@ if __name__ == '__main__':
  N,M = 200,3
 
  Head = {'algo':algo,'conf':"T0,alpha,P,L = {0},{1},{2},{3}".format(conf[0],conf[1],conf[2],conf[3]),'dim':dim,"dataset":dataset}
-
- oc = oct2py.Oct2Py()
+ try:
+  oc = oct2py.Oct2Py()
+ except:
+  oc = oct2py.Oct2Py()
  oc.addpath("common_HF")
  atexit.register(oc.exit)
  
@@ -115,6 +120,7 @@ if __name__ == '__main__':
   k = int(round(Nc*args[1]))
   num_start = int(round(args[2]))
   thre = args[3]
+  beta = args[4]
   search_step = 1
 
   Y,names = [],[]
@@ -126,7 +132,7 @@ if __name__ == '__main__':
     names.append(dataset+"/"+i)
   N = len(names)
 
-  print "N = {0}, Ncpu = {1}, Nc = {2}, k = {3}, thre = {4}, ns = {5}, ss = {6}".format(N,Ncpu,Nc,k,thre,num_start,search_step)
+  print "N = {0}, Ncpu = {1}, Nc = {2}, k = {3}, thre = {4}, ns = {5}, ss = {6}, beta = {7}".format(N,Ncpu,Nc,k,thre,num_start,search_step,beta)
   #print "{0} {1} {2} {3} {4}".format(Nc,k,thre,num_start,search_step)    
   limits_hi= np.linspace(2*N/Ncpu,N,Ncpu/2).astype(int)
   limits_lo = np.hstack((0,limits_hi[0:limits_hi.shape[0]-1]))
@@ -144,7 +150,7 @@ if __name__ == '__main__':
    b = b+i[1]
 
   Fl1,Fl2 = [],[]
-
+  C1,C2 = [],[]
   #print "Suavizando"
  
   if k > 1:
@@ -153,22 +159,27 @@ if __name__ == '__main__':
    idx =[np.arange(lo,hi) for lo,hi in zip(limits_lo[0:len(limits_lo)-1],limits_hi)]
    for mt in a:
     F = np.array([[k[i].mean() for i in idx] for k in mt.T])
-    F = F/np.tile(F.max(axis = 0),(Nc,1))
+    
+    F = F/np.tile(np.abs(F).max(axis = 0),(Nc,1))
+    C1.append(F.std(axis = 0).mean())
     Fl1.append(F.T)
    for mt in b:
-    F = np.array([[k[i].mean() for i in idx] for k in mt.T])
-    F = F/np.tile(F.max(axis = 0),(Nc,1)) 
+    F = np.array([[k[i].mean() for i in idx] for k in mt.T])  
+    F = F/np.tile(np.abs(F).max(axis = 0),(Nc,1))
+    C2.append(F.std(axis = 0).mean()) 
     Fl2.append(F.T)
   else:
    for mt in a:
-    F = mt.T/np.tile(mt.T.max(axis = 0),(Nc,1))
-    Fl1.append(F)
+    F = mt.T/np.tile(np.abs(mt.T).max(axis = 0),(Nc,1))
+    C1.append(F.std(axis = 0).mean()) 
+    Fl1.append(F.T)
    for mt in b:
-    F = mt.T/np.tile(mt.T.max(axis = 0),(Nc,1))
-    Fl2.append(mt)
+    F = mt.T/np.tile(np.abs(mt.T).max(axis = 0),(Nc,1))
+    C2.append(F.std(axis = 0).mean())
+    Fl2.append(F)
   #print "Calculando md"
   oc.clear()
-  md = oc.pdist2(Fl1,Fl2,thre,num_start,search_step)
+  md = oc.pdist2(Fl1,Fl2,C1,C2,thre,num_start,search_step,beta)
   #print "Calculando Silhouette"
   cost = float(np.median(1. - silhouette(md,np.array(Y)-1)))
   #print